f77c9e9b18e41fb6fc230f8d77609756939bf0e8
mbagro <44054417+mbagro@users.noreply.github.com>
2018-11-04 19:59:42 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1a-e8lDMJ2d3KHEgBfU89dzTNAh-m3sETvMcCnoKLn1c/edit#gid=0';
